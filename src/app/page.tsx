14dc59c4ae0e796e644c1639565ec9e987d4df5d
Ideguchi_Pharma <ideguchi@pharmacloud.jp>
2025-07-07 09:25:06 +0900

    delete RiinformationFill className

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
             マイページ ・ メドサーチ
         </p>
         <div className="flex items-start space-x-2 px-4 py-2 rounded-lg" style={{ backgroundColor: "#e0f7fa" }}>
-         <RiInformationFill className="w-5 h-5 text-cyan-600 flex-shrink-0 mt-0.5" /> 
-        <p className="text-black px-0.5 py-0.5 text-sm text-gray-800 rounded font-[family-name:var(--font-geist-mono)] flex-grow]">
+         <RiInformationFill /* className="w-5 h-5 text-cyan-600 flex-shrink-0 mt-0.5" */ />
+         <p className="text-black px-0.5 py-0.5 text-sm text-gray-800 rounded font-[family-name:var(--font-geist-mono)] flex-grow]">
             グループに共有されている在庫状況を検索します。対象グループを選択して、医薬品名を入力してください。
         </p>
         </div>
